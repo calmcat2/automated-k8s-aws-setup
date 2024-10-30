99860b7c66886ffec05f12379540b80f5474a392
Maxine <hmx222@gmail.com>
2024-10-30 17:06:08 -0600

    first commit

--- Terraform1/variables.tf
+++ Terraform1/variables.tf
@@ -1,5 +1,7 @@
-variable "region"{
-  default = "us-east-1"
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
   nullable = false
 }
 
